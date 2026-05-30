ab144a56fab5f80119157007a5b3e9eca4469d30
LidiyaShamalo <arkteya07@gmail.com>
2026-05-30 17:00:24 +0300

    data representation structures

---
+++ lib/chat/messages.ex
@@ -0,0 +1,16 @@
+defmodule Chat.Message do
+
+  defmodule Register do
+    @type t() :: %__MODULE__{username: String.t()}
+    defstruct [:username]
+  end
+
+  defmodule Broadcast do
+    @type t() :: %__MODULE__{
+      from_username: String.t(),
+      contents: String.t()
+    }
+    defstruct [:from_username, :contents]
+  end
+
+end

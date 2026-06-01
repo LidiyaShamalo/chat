aef831d408e43ac6229dda70b6e422469a5d27e1
LidiyaShamalo <arkteya07@gmail.com>
2026-06-01 10:54:25 +0300

    creating reception logic

--- lib/chat/application.ex
+++ lib/chat/application.ex
@@ -10,6 +10,7 @@ defmodule Chat.Application do
     children = [
       # Starts a worker by calling: Chat.Worker.start_link(arg)
       # {Chat.Worker, arg}
+      {Chat.Acceptor, port: 4000}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html

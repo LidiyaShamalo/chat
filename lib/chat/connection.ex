bd7f92683d94a56f310aaaa3b0509dc6f1dd901a
LidiyaShamalo <arkteya07@gmail.com>
2026-06-01 11:38:20 +0300

    connection handling during user registration

---
+++ lib/chat/connection.ex
@@ -0,0 +1,60 @@
+defmodule Chat.Connection do
+  use GenServer, restart: :temporary
+
+  @spec start_link(:gen_tcp.socket()) :: GenServer.on_start()
+  def start_link(socket) do
+    GenServer.start_link(__MODULE__, socket)
+  end
+
+  defstruct [:socket, :username, buffer: <<>>]
+
+  @impl true
+  def init(socket) do
+    {:ok, %__MODULE__{socket: socket}}
+  end
+
+  @impl true
+  def handle_info(message, state)
+
+  def handle_info(
+    {:tcp, socket, data},
+    %__MODULE__{socket: socket} = state
+  ) do
+    state = update_in(state.buffer, &(&1 <> data))
+    :ok = :inet.setopts(socket, active: :once)
+    handle_new_data(state)
+  end
+
+  ## Helpers
+
+  defp handle_new_data(state) do
+    case Chat.Protocol.decode_message(state.buffer) do
+      {:ok, message, rest} ->
+        state = put_in(state.buffer, rest)
+
+        case handle_message(message, state) do
+          {:ok, state} -> handle_new_data(state)
+          :error  -> {:stop, :normal, state}
+        end
+
+      :incomplete ->
+        {:noreply, state}
+
+      :error ->
+        Logger.error("Received invalid data, closing connection")
+        {:stop, :normal, state}
+    end
+  end
+
+  defp handle_message(
+    %Register{username: username},
+    %__MODULE__{username: nil} = state
+  ) do
+    {:ok, put_in(state.username, username)}
+  end
+
+  defp handle_message(%Register{}, _state) do
+    Logger.error("Invalid Register message, had already received one")
+    :error
+  end
+end
